507010911a2d99202678a1681df0b5950aa7dce6
andreasgiannoudis <agiannoudis@hotmail.com>
2024-02-10 12:05:26 +0100

    fixed a bug

--- src/modules/typedAnimation.js
+++ src/modules/typedAnimation.js
@@ -2,12 +2,22 @@ import Typed from 'typed.js';
 
 export function typedAnimation(selector, translationKeys, typeSpeed, backSpeed, backDelay, loop, languageData, currentLanguage) {
   const strings = translationKeys.map(key => languageData[currentLanguage][key]);
-
-  return new Typed(selector, {
-    strings,
-    typeSpeed,
-    backSpeed,
-    backDelay,
-    loop,
-  });
+  let currentIndex = 0;
+  const typeNextWord = () => {
+    if (currentIndex < strings.length) {
+      const typedInstance = new Typed(selector, {
+        strings: [strings[currentIndex]],
+        typeSpeed,
+        backSpeed,
+        backDelay,
+        loop,
+        onComplete: () => {
+          currentIndex++;
+          typeNextWord();
+        }
+      });
+    }
+  };
+  // Start typing the first word asynchronously
+  setTimeout(typeNextWord, 0);
 }
