d18608a9fcc09e0608a1e8427d06e943985b1a02
andreasgiannoudis <agiannoudis@hotmail.com>
2024-02-10 12:53:46 +0100

    .

--- src/modules/typedAnimation.js
+++ src/modules/typedAnimation.js
@@ -1,6 +1,7 @@
 import Typed from 'typed.js';
 
 let typeInstance = null;
+let throttleTimer = null;
 
 export function typedAnimation(selector, translationKeys, typeSpeed, backSpeed, backDelay, loop, languageData, currentLanguage) {
   // Check if there's an existing typing instance, destroy it if present
@@ -17,8 +18,19 @@ export function typedAnimation(selector, translationKeys, typeSpeed, backSpeed, 
     backSpeed,
     backDelay,
     loop,
+    // Callback function to throttle DOM updates
+    onStringTyped: function() {
+      if (!throttleTimer) {
+        throttleTimer = setTimeout(() => {
+          throttleTimer = null;
+          updatePageLanguage();
+        }, 100); // Adjust the throttle duration as needed
+      }
+    }
   });
 
   // Return the typing instance
   return typeInstance;
 }
+
+// Your other code remains the same
