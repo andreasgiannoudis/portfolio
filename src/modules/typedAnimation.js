e50d5fd081e5d09f333fabd411440c558e74d76c
andreasgiannoudis <agiannoudis@hotmail.com>
2024-02-10 12:06:49 +0100

    .

--- src/modules/typedAnimation.js
+++ src/modules/typedAnimation.js
@@ -2,22 +2,12 @@ import Typed from 'typed.js';
 
 export function typedAnimation(selector, translationKeys, typeSpeed, backSpeed, backDelay, loop, languageData, currentLanguage) {
   const strings = translationKeys.map(key => languageData[currentLanguage][key]);
-  let currentIndex = 0;
-  const typeNextWord = () => {
-    if (currentIndex < strings.length) {
-      const typedInstance = new Typed(selector, {
-        strings: [strings[currentIndex]],
-        typeSpeed,
-        backSpeed,
-        backDelay,
-        loop,
-        onComplete: () => {
-          currentIndex++;
-          typeNextWord();
-        }
-      });
-    }
-  };
-  // Start typing the first word asynchronously
-  setTimeout(typeNextWord, 0);
+
+  return new Typed(selector, {
+    strings,
+    typeSpeed,
+    backSpeed,
+    backDelay,
+    loop,
+  });
 }
